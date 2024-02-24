7ce95b26f2231fef0d6d18bcbca13e1dd8770084
HarshalGunjalOp <hrshlgunjal3@gmail.com>
2024-02-24 20:09:18 +0530

    initial commit

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import {DB_NAME} from '../constants.js';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: DB_NAME,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection FAILED: ', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
