043909f3d3142622907907c7bd83ff4117e725d5
HarshalGunjalOp <hrshlgunjal3@gmail.com>
2024-02-26 14:06:55 +0530

    initial commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
-import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import JWT from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema(
     {
@@ -46,5 +47,46 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre("save", async function(next){
+    if (this.isModified("password")){
+        this.password = bcrypt.hash(this.password, 10);
+    }
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return JWT.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return JWT.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+}
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const videoSchema = new mongoose.Schema(
     {
@@ -43,5 +44,7 @@ const videoSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
